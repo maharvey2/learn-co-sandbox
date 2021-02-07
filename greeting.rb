62b5691bb167587cbe0e65546b5272d99b209bc1
Mark Harvey <mah0013mobile@gmail.com>
2021-02-07 16:28:35 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,4 @@
+#             assigning a default value
+def greeting(name = "Ruby programmer")
+  puts "Hello, #{name}"
+end
---
+++ vehicle_methods.rb
@@ -0,0 +1,14 @@
+def accelerate
+  puts "Stepping on the gas!"
+  puts "Speeding up"
+end
+
+def sound_horn
+  puts "Pressing the horn!"
+  puts "Beep Beep!"
+end
+
+def use_headlights(brightness = "low-beam")
+  puts "Turning on #{brightness} Headlights"
+  puts "watchout for deer!"
+end
